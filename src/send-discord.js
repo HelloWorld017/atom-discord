f4094544e80a75d2a1e3721a3ba8584704b350a1
Khinenw <deu07115@gmail.com>
2017-11-25 09:01:51 +0900

    Fixed #3

--- src/send-discord.js
+++ src/send-discord.js
@@ -24,6 +24,23 @@ const getTranslation = (key, args = {}) => {
 	return tr;
 };
 
+if (!String.prototype.padStart) {
+    String.prototype.padStart = function padStart(targetLength,padString) {
+        targetLength = targetLength>>0; //floor if number or convert non-number to 0;
+        padString = String(padString || ' ');
+        if (this.length > targetLength) {
+            return String(this);
+        }
+        else {
+            targetLength = targetLength-this.length;
+            if (targetLength > padString.length) {
+                padString += padString.repeat(targetLength/padString.length); //append to original to ensure we are longer than needed
+            }
+            return padString.slice(0,targetLength) + String(this);
+        }
+    };
+}
+
 const setupRpc = (Client) => {
 	rpc = new Client({ transport: 'ipc' });
 
